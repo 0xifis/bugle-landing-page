d93a33dd501cdbd6231c0c102b091d13e2f9a7f5
Vishnu R Menon <vishthemenon@gmail.com>
2016-06-07 19:10:28 +0530

    added opacity gradient to svg circles; removed random fadein delay

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 $(function() {
-  var num = 25
+  var num = 30
   for(var i = 0; i < num; i++) {
     var j = Math.floor(200/num)
     var radius = i*j + Math.floor(Math.random()*j)
     var stroke = Math.floor(radius*0.2 + Math.random()*radius*0.7)
     var offset = Math.floor(radius*0.2 + Math.random()*radius*0.7)
+    var opacity = 0.8*(1 - 1/num*i)
     var animation = Math.floor(Math.random()*2)
     var duration = Math.floor(Math.random()*20) + 5
     animation == 1 ? animation = "clockwise" : animation = "anti-clockwise"
-    $('.bg').append('<circle class="bg-circle" cx="50" cy="50" r="'+radius+'" style="stroke-dasharray: '+ stroke +'; stroke-dashoffset: '+ offset + '; animation-name: ' + animation + '; animation-duration: ' + duration + 's; "/>')
+    $('.bg').append('<circle class="bg-circle" cx="50" cy="50" r="'+radius+'" style="stroke-dasharray: '+ stroke +'; stroke-dashoffset: '+ offset + '; animation-name: ' + animation + '; animation-duration: ' + duration + 's; opacity: ' + opacity + ';"/>')
   }
   $(".bg").html($(".bg").html());
   for(var j = 0; j < num; j++) {
-    $('circle:nth-of-type('+j+')').delay(j*Math.random()*500).fadeIn("slow");
+    $('circle:nth-of-type('+j+')').delay(j*200).fadeIn("slow");
   }
 })
 
